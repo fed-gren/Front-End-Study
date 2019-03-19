416b34cac650578057d5a91f59f24e406480b828
fed-gren <deveb22@gmail.com>
2019-03-19 16:32:33 +0900

    예제 수정

--- React/hello-react/src/App.js
+++ React/hello-react/src/App.js
@@ -1,14 +1,18 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import MyComponent from './MyComponent';
-import EventPractice from "./EventPractice";
+import ScrollBox from './ScrollBox';
+import Test from "./Test";
 
 class App extends Component {
   render() {
     return (
       // <MyComponent name="EBL" age={28}/>
-      <Fragment>
-        <EventPractice />
-      </Fragment>
+      <>
+        <ScrollBox ref={(ref) => { this.scrollBox = ref }} />
+        <button onClick={() => this.scrollBox.scrollToBottom()}>맨 밑으로</button>
+        {/* <button onClick={this.scrollBox.scrollToBottom}>맨 밑으로</button> */}
+        <Test></Test>
+      </>
     );
   }
 }
