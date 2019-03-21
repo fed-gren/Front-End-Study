a1893691084a9a1e1fb3ad1eab0d2b6f7b107291
fed-gren <deveb22@gmail.com>
2019-03-22 01:01:30 +0900

    07 컴포넌트 라이프 사이클 실습

--- React/hello-react/src/App.js
+++ React/hello-react/src/App.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
 import EventPractice from './EventPractice';
 import ScrollBox from './ScrollBox';
-import InteractionSample from './InteractionSample'
+import InteractionSample from './InteractionSample';
+import LifeCycleSample from './LifeCycleSample';
 import Test from "./Test";
 
+function getRandomColor() {
+  return `#${Math.floor(Math.random() * 1677215).toString(16)}`
+}
+
 class App extends Component {
+
+  state = {
+    color: '#000000'
+  }
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor()
+    });
+  }
+
   render() {
     return (
       // <MyComponent name="EBL" age={28}/>
@@ -17,6 +33,9 @@ class App extends Component {
         {/* <button onClick={this.scrollBox.scrollToBottom}>맨 밑으로</button> */}
         <h2>컴포넌트 반복 - map</h2>
         <InteractionSample/>
+        <h2>라이프 사이클</h2>
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <LifeCycleSample color={this.state.color}/>
         <h2>Test</h2>
         <Test></Test>
       </>
