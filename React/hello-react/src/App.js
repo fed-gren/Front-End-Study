081136286204435fb3ad419295930851e6b675a3
fed-gren <deveb22@gmail.com>
2019-03-29 00:55:57 +0900

    CSS Module, Sass 사용 컴포넌트 스타일링

--- React/hello-react/src/App.js
+++ React/hello-react/src/App.js
@@ -1,94 +1,108 @@
 import React, { useState, useEffect } from 'react';
 import Axios from 'axios';
-
-// class App extends Component {
-//   state = {
-//     count: 0
-//   }
-//   handleClick = () => {
-//     this.setState({
-//       count: this.state.count + 1
-//     })
-//   }
-
-//   render() {
-//     return (
-//       <>
-//       <div>count : {this.state.count}</div>
-//       <button onClick={() => this.handleClick()}>더하기</button>
-//       </>
-//     );
-//   }
-// }
-
-// ! Using React Hooks
-function useInput(defaultValue) {
-  const [value, setValue] = useState(defaultValue);
-  const onChange = (e) => {
-    const {target :{value}} = e;
-    setValue(value);
-  };
-
-  return { value, onChange };
-}
-
-function useFetch(url) {
-  const [payload, setPayload] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState("");
-
-  const callUrl = async () => {
-    try {
-      const { data } = await Axios.get(url);
-      setPayload(data);
-    } catch {
-      setError("\u{1F622}");
-    } finally {
-      setLoading(false);
-    }
-  }
-
-  useEffect(() => {   //did mount와 같은 동작
-    callUrl();
-  }, []);
-
-  return {payload, loading, error};
-}
+import classNames from 'classnames/bind';
+import styles from './App.scss';
+const cx = classNames.bind(styles);
 
 const App = () => {
-  const [count, setCount] = useState(0);
-  const [email, setEmail] = useState("");
-  const updateEmail = e => {
-    const {
-      target: {value}
-    } = e;
-    console.log(e.target.value);
-    setEmail(value);
-  }
-
-  const name = useInput("");
-  console.log(name);
-
-  const {payload, loading, error} = useFetch('http://aws.random.cat/meow');
+  const isBlue = true;
   return (
-    <>
-    <h2>React hooks 사용법</h2>
-      <div>{count}</div>
-      <button onClick={() => setCount(count + 1)}>더하기</button>
-      <button onClick={() => setCount(count - 1)}>빼기</button>
-      <input placeholder="Email" value={email} onChange={updateEmail}/>
+    <div className={cx('box', {
+      blue: isBlue
+    })}>
+      <div className={cx('box-inside')} />
+    </div>
+  );
 
-      <hr/>
-      <h2>input</h2>
-      
-      <input {...name} placeholder="what's your name"/>
-      <hr/>
-      <h2>fetch - api에서 데이터 쉽게 가져오기</h2>
-      {loading && <span>Loading your cat...</span>}
-      {!loading && error && <span>{error}</span>}
-      {!loading && !error && <img src={payload.file} width="150"></img>}
-    </>
-  )
+  // class App extends Component {
+  //   state = {
+  //     count: 0
+  //   }
+  //   handleClick = () => {
+  //     this.setState({
+  //       count: this.state.count + 1
+  //     })
+  //   }
+
+  //   render() {
+  //     return (
+  //       <>
+  //       <div>count : {this.state.count}</div>
+  //       <button onClick={() => this.handleClick()}>더하기</button>
+  //       </>
+  //     );
+  //   }
+  // }
+
+  // ! Using React Hooks
+  // function useInput(defaultValue) {
+  //   const [value, setValue] = useState(defaultValue);
+  //   const onChange = (e) => {
+  //     const {target :{value}} = e;
+  //     setValue(value);
+  //   };
+
+  //   return { value, onChange };
+  // }
+
+  // function useFetch(url) {
+  //   const [payload, setPayload] = useState(null);
+  //   const [loading, setLoading] = useState(true);
+  //   const [error, setError] = useState("");
+
+  //   const callUrl = async () => {
+  //     try {
+  //       const { data } = await Axios.get(url);
+  //       setPayload(data);
+  //     } catch {
+  //       setError("\u{1F622}");
+  //     } finally {
+  //       setLoading(false);
+  //     }
+  //   }
+
+  //   useEffect(() => {   //did mount와 같은 동작
+  //     callUrl();
+  //   }, []);
+
+  //   return {payload, loading, error};
+  // }
+
+  // const App = () => {
+  //   const [count, setCount] = useState(0);
+  //   const [email, setEmail] = useState("");
+  //   const updateEmail = e => {
+  //     const {
+  //       target: {value}
+  //     } = e;
+  //     console.log(e.target.value);
+  //     setEmail(value);
+  //   }
+
+  //   const name = useInput("");
+  //   console.log(name);
+
+  //   const {payload, loading, error} = useFetch('http://aws.random.cat/meow');
+  //   return (
+  //     <>
+  //     <h2>React hooks 사용법</h2>
+  //       <div>{count}</div>
+  //       <button onClick={() => setCount(count + 1)}>더하기</button>
+  //       <button onClick={() => setCount(count - 1)}>빼기</button>
+  //       <input placeholder="Email" value={email} onChange={updateEmail}/>
+
+  //       <hr/>
+  //       <h2>input</h2>
+
+  //       <input {...name} placeholder="what's your name"/>
+  //       <hr/>
+  //       <h2>fetch - api에서 데이터 쉽게 가져오기</h2>
+  //       {loading && <span>Loading your cat...</span>}
+  //       {!loading && error && <span>{error}</span>}
+  //       {!loading && !error && <img src={payload.file} width="150"></img>}
+  //     </>
+  // )
+
 }
 
 export default App;
